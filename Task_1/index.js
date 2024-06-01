737b1165c81a694b9416268862e520a64450c0a7
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-01 13:59:11 +0000

    feat: add sign up

--- Task_1/index.js
+++ Task_1/index.js
@@ -2,6 +2,10 @@
 const http = require('http');
 const url = require('url');
 const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+
 
 const server = http.createServer();
 
@@ -10,6 +14,134 @@ const port = 9000;
 server.listen(port, () => {
     console.log(`Running server on port ${port}...`)
 })
+
+server.on('request', (req, res) => {
+
+    // MIDDLEWARE SHOULD BE HERE
+
+    
+    
+    
+    if(req.method === 'POST' && req.url === '/') {
+        let data = '';
+        req.on('data', (chunk) => {
+            data = chunk;
+        }).on('end', () => {
+            const reqData = JSON.parse(data.toString());
+            const {username, password} = reqData;
+            if (username !== 'admin' || password !== 'password') {
+                res.statusCode = 401;
+                return res.end(JSON.stringify({
+                    message: 'Authentication required'
+                }))
+            }
+            const authKey = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+            res.setHeader('Authorization', authKey);
+            return res.end(JSON.stringify({
+                message: 'User authenticated'
+            }));
+        })
+    }
+
+    // sign up
+    if(req.method === 'POST' && req.url === '/signup') {
+        let data = '';
+        req.on('data', (chunk) => {
+            data = chunk;
+        }).on('end', () => {
+            const reqData = JSON.parse(data.toString());
+            const {username, email, password} = reqData;
+            if (!username || !password || !email) {
+                res.statusCode = 400;
+                return res.end(JSON.stringify({
+                    message: 'Incomplete registration'
+                }))
+            }
+            const authKey = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+            res.setHeader('Authorization', authKey);
+            return res.end(JSON.stringify({
+                message: 'User authenticated'
+            }));
+        })
+    }
+    // GET MEMORIES
+    // const link = url.parse(req.url);
+    // console.log(link);
+
+    
+    else if (req.method === 'GET' && req.url === '/memories') {
+        // authentication check
+        const key = req.headers.authorization;
+        // console.log(key);
+        if (!key) {
+            return res.end(JSON.stringify({
+                message: 'Authentication required'
+            }))
+        }
+
+        
+        let data = '';
+        const location = __dirname + '\\' + 'data.json';
+        req.on('data', (chunk) => {
+            data = chunk;
+        })
+        .on('end', () => {
+            const reader = fs.createReadStream(location); 
+            reader.on('data', (chunk) => {
+                data = chunk;
+            })
+            .on('end', () => {
+                const memory = JSON.parse(data.toString());
+                res.end(JSON.stringify(memory));
+            })
+        })
+    }
+    // POST MEMORIES
+    else if (req.method === 'POST' && req.url === '/memories') {
+
+                req.on('data', (chunk) => {
+            const data = chunk.toString();
+            const { content } = JSON.parse(data);
+
+            const newPost = {
+                id: generateUniqueIds(),
+                content,
+            }
+
+            const filePath = path.join(__dirname, 'data.json');
+            // read the file to get the content 
+            let contents = '';
+            const reader = fs.createReadStream(filePath);
+            reader.on('data', (chunk) => {
+                contents = chunk;
+            })
+            .on('end', () => {
+                let data = JSON.parse(contents.toString());
+                // console.log('data: ', data);
+                const collection = [...data, newPost];
+                // console.log('collection: ', collection);
+
+                // writing into file
+                const writer = fs.createWriteStream(filePath);
+                writer.write(JSON.stringify(collection));
+
+            })
+            
+            
+        })
+        res.end(JSON.stringify({
+            message: 'memory published successfully',
+        }))
+    }
+    else {
+        res.statusCode = 404;
+        res.end(JSON.stringify({
+            message: 'Not Found'
+        }))
+    }
+})
+
+
 
 
 
@@ -125,22 +257,6 @@ server.listen(port, () => {
     
 // })
 
-// /**generate id based on timestamp
-//  * concatenate today's date and time stamp
-//  * get day, month, year, and timestamp
-//  */
-// const generateUniqueIds = function() {
-
-//     const date = new Date();
-//     const timestamp = Date.now(); // timestamp
-    
-//     const day = date.getDate().toString().padStart(2, 0);
-//     const month = (date.getMonth() + 1).toString().padStart(2, 0);
-//     const year = date.getFullYear();
-
-//     return Number(`${day}${month}${year}${timestamp}`)
-    
-// }
 
 
 
@@ -174,4 +290,20 @@ server.listen(port, () => {
 // before users can view or post they need to be authenticated
 // another middleware to check it the url params is admin
 
-// git remote add origin git@github.com:Jhay-Great/kodecamp.git
+/**generate id based on timestamp
+ // git remote add origin git@github.com:Jhay-Great/kodecamp.git
+ * concatenate today's date and time stamp
+ * get day, month, year, and timestamp
+ */
+const generateUniqueIds = function() {
+
+    const date = new Date();
+    const timestamp = Date.now(); // timestamp
+    
+    const day = date.getDate().toString().padStart(2, 0);
+    const month = (date.getMonth() + 1).toString().padStart(2, 0);
+    const year = date.getFullYear();
+
+    return Number(`${day}${month}${year}${timestamp}`)
+    
+}
