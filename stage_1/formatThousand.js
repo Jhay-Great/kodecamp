95cec7916f2ae1b5fe3be3939edbaba0e604c315
Jhay-Great <alemonai.jenef@gmail.com>
2024-06-05 14:36:29 +0000

    updated function formatThousand.js

--- stage_1/formatThousand.js
+++ stage_1/formatThousand.js
@@ -1,22 +1,5 @@
 const formatThousand = function(number) {
-    /**format with commas
-     * loop through the input or number
-     * count from right to left
-     * add comma after every 3 digits counted
-     * 
-     * so:
-     * loop would start at the length of input
-     * condition for the loop would be initializer greater than 0
-     * loop would be decreasing
-     * 
-     * for or while loop alt. recursive
-     * 
-     * alt2.0
-     * initialize a count variable
-     * increment count per every push
-     * when count is 3 add a comma
-     */
-
+    
     let num = number.toString();
     let formatted = [];
     
@@ -25,18 +8,15 @@ const formatThousand = function(number) {
 
         if (count % 3 === 0 && count !== 0) {
             formatted.push(',');
-            // continue;
         }
         
         formatted.push(num[i]);
         count++;
     }
 
-    console.log(formatted.reverse().join(''));
-
-    
+    return formatted.reverse().join('');
 }
 
 
 
-formatThousand(8832);
+console.log(formatThousand(10000));
