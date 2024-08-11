9f8d5c01996c4ca4bd05a0f1e4f51df5ea8fe90f
Jhay-Great <alemonai.jenef@gmail.com>
2024-08-11 15:20:01 +0000

    adds descriptive error response

--- stage_6/src/middlewares/verifyUserLogin.js
+++ stage_6/src/middlewares/verifyUserLogin.js
@@ -6,19 +6,21 @@ const verifyUserLogin = async function (req, res, next) {
     const { email, password } = req.body;
 
     const user = await findUserByEmail(email);
+    // console.log(user);
     
 
     if (user === null) return res.status(401).json({
         error: true,
-        message: 'User does not exist',
+        message: 'Incorrect email address, User does not exist',
     });
     console.log('email passed...')
     
     const result = await comparePassword(password, user.password)
+    console.log(result);
     
     if (!result)  return res.status(401).json({
         error: true,
-        message: 'User does not exist',
+        message: 'Incorrect password, User does not exist',
     }); ;
     console.log('password passed...')
     
